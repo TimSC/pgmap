87ec9369b3b5410f81c1860d1df62c85761f3774
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2019-04-17 13:39:37 +0100

    Remove query duplicates using filter #5

--- dbfilters.h
+++ dbfilters.h
@@ -46,5 +46,30 @@ public:
 		const std::vector<std::string> &refRoles);
 };
 
+class FilterObjectsUnique : public IDataStreamHandler
+{
+public:
+	FilterObjectsUnique(std::shared_ptr<IDataStreamHandler> enc);
+	virtual ~FilterObjectsUnique();
+
+	virtual void Sync();
+	virtual void Reset();
+	virtual void Finish();
+
+	virtual void StoreIsDiff(bool isDiff);
+	virtual void StoreBounds(double x1, double y1, double x2, double y2);
+	virtual void StoreNode(int64_t objId, const class MetaData &metaData, 
+		const TagMap &tags, double lat, double lon);
+	virtual void StoreWay(int64_t objId, const class MetaData &metaData, 
+		const TagMap &tags, const std::vector<int64_t> &refs);
+	virtual void StoreRelation(int64_t objId, const class MetaData &metaData, const TagMap &tags, 
+		const std::vector<std::string> &refTypeStrs, const std::vector<int64_t> &refIds, 
+		const std::vector<std::string> &refRoles);
+
+private:
+	std::set<int64_t> nodeIds, wayIds, relationIds;
+	std::shared_ptr<IDataStreamHandler> enc;
+};
+
 #endif //_DB_FILTERS_H
 
